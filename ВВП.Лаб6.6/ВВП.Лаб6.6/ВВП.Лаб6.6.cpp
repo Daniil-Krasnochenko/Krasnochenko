509411fa49380adced5912c92feedfb0410219a8
Daniil-Krasnochenko <90762603+Daniil-Krasnochenko@users.noreply.github.com>
2021-10-13 13:45:40 +0300

    Add files via upload

---
+++ ВВП.Лаб6.6/ВВП.Лаб6.6/ВВП.Лаб6.6.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int A, B;
+    setlocale(0, "russian");
+    cout << "Введите значение A\n";
+    cin >> A;
+    B = A * A;
+    A = B * B;
+    B = A * A;
+    cout << B;
+    return 0;
+}
---
+++ ВВП.Лаб6.7/ВВП.Лаб6.7/ВВП.Лаб6.7.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int A, B,C;
+    setlocale(0, "russian");
+    cout << "Введите значение A \n";
+    cin >> A;
+    B = A * A;
+    C = A * B;
+    A = B * C;
+    B = A * A;
+    C = A * B;
+    cout << C;
+    return 0;
+}
