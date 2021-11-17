86be654f0c65d44867c60a314d52dfc8d7fed621
Daniil-Krasnochenko <90762603+Daniil-Krasnochenko@users.noreply.github.com>
2021-11-17 13:29:32 +0300

    Add files via upload

---
+++ ВВП.Лаб7.2/ВВП.Лаб7.2/ВВП.Лаб7.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    cout << "Введите величину угла в радианах: \n";
+    double pi = 3.1415926, a;
+    cin >> a;
+    cout << "Угол равен  ";
+    cout << "a1 = " << a / (pi / 180);
+    cout << " градусов ";
+
+    return 0;
+}
---
+++ ВВП.Лаб7.4/ВВП.Лаб7.4/ВВП.Лаб7.4.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double S, V1, V2, T ;
+	setlocale(LC_ALL, "");
+	cout << "Введите значение скоростей V1 км/ч, V2 км/ч, начального расстояния S км и времени T часов:\n ";
+	cin >> V1 >> V2 >> S >> T;
+	cout << "Расстояние между двумя автомобилями равно " << (V1 + V2) * T + S << " км";
+	return 0;
+}
