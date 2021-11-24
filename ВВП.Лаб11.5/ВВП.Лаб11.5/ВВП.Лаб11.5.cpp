143188a8681ac7af86e7d6a641a22398af7aea41
Daniil-Krasnochenko <90762603+Daniil-Krasnochenko@users.noreply.github.com>
2021-11-24 12:19:57 +0300

    Add files via upload

---
+++ ВВП.Лаб11.5/ВВП.Лаб11.5/ВВП.Лаб11.5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "");
+	cout << "Введите целое число:\n ";
+	int a;
+	cin >> a;
+	setlocale(LC_ALL, "");
+	if (a > 0 && a % 2 == 0) {
+		cout << "Положительное четное число " << endl;
+	}
+	if (a > 0 && a % 2 == 1) {
+		cout << "Положительное нечетное число " << endl;
+	}
+	if (a < 0 && a % 2 == 0) {
+		cout << "Отрицательное четное число " << endl;
+	}
+	if (a < 0 && a % 2 == 1) {
+		cout << "Отрицательное нечетное число " << endl;
+	}
+	if (a == 0) {
+		cout << "Нулевое число " << endl;
+	}
+	return 0;
+}
