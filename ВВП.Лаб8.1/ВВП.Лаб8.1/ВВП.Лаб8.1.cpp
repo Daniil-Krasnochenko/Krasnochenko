4ae6880d0f8777fdfeef11eb190698439f4c0124
Daniil-Krasnochenko <90762603+Daniil-Krasnochenko@users.noreply.github.com>
2021-11-24 12:01:13 +0300

    Add files via upload

---
+++ ВВП.Лаб8.1/ВВП.Лаб8.1/ВВП.Лаб8.1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    cout << "Введите размер файла в байтах:" << endl;
+    int  a;
+    cin >> a;
+    cout << "Размер в килобайтах равен\n";
+    if( a%1024==0)
+        cout << a / 1024  ;
+    else
+        cout << a / 1024 + 1;
+    return 0;
+}
---
+++ ВВП8.2/ВВП8.2/ВВП8.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    cout << "Введите величину отрезков А и В:" << endl;
+    int  a, b, z = 0;
+    cin >> a >> b;
+    cout << "количество отрезков B, размещенных на отрезке A: ";
+    for (int m = a; m >= b; m -= b) {
+        z++;
+    }
+    cout << z;
+    return 0;
+}
+
