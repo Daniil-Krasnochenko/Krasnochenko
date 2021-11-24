92bbe1422c7b4b40812ad72827ecb6a3bfff9772
Daniil-Krasnochenko <90762603+Daniil-Krasnochenko@users.noreply.github.com>
2021-11-24 12:13:49 +0300

    Add files via upload

---
+++ ВВП.Лаб11.1/ВВП.Лаб11.1/ВВП.Лаб11.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    cout << "Введите 2 числа: \n";
+    int  a, b;
+    cin >> a >> b;
+    if (a > b) {
+        b = a;
+        cout << "Числу В присвоено значение А : ";
+        cout << a << " " << b;
+    }
+    else if (a < b) {
+        a = b;
+        cout << "Числу А присвоено значение В : ";
+        cout << a << " " << b;
+    }
+    else {
+        a = b = 0;
+        cout << "Числам В и А присвоено значение 0 : ";
+        cout << a << " " << b;
+    }
+    return 0;
+}
---
+++ ВВП.Лаб11.2/ВВП.Лаб11.2/ВВП.Лаб11.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "");
+	cout << "Введите 3 числа : \n";
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a > b && c > b) {
+		cout << "a + c = " << a + c << endl;
+	}
+	else if (b > a && c > a) {
+		cout << "b + c = " << b + c << endl;
+	}
+	else if (b > c && a > c) {
+		cout << "b + a = " << b + a << endl;
+	}
+	return 0;
+}
