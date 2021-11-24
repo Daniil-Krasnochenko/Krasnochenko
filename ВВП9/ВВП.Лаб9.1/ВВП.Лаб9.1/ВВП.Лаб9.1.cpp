887bc2773cc581d48a3c0469b4510efb9f4da40e
Daniil-Krasnochenko <90762603+Daniil-Krasnochenko@users.noreply.github.com>
2021-11-24 12:04:53 +0300

    Add files via upload

---
+++ ВВП9/ВВП.Лаб9.1/ВВП.Лаб9.1/ВВП.Лаб9.1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    cout << "С начала суток прошло секунд :" << endl;
+    int x;
+    cin >> x;
+    cout << "Количество секунд, прошедших с начала последней минуты: ";
+    cout << x % 60;
+    return 0;
+}
+
