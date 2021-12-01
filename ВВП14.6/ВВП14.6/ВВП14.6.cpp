700be124c900894a4209834e4ace10d186a76466
Daniil-Krasnochenko <90762603+Daniil-Krasnochenko@users.noreply.github.com>
2021-12-01 13:34:40 +0300

    Add files via upload

---
+++ ВВП14.6/ВВП14.6/ВВП14.6.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "");
+	cout << "Введите число последовательности Фибоначи : ";
+	int a = 0, b = 1, N, q = 0, m = 2, w;
+	cin >> N;
+	while (b != N) {
+		q = b;
+		b = a + b;
+		a = q;
+		m++;
+	}
+	cout << "Номер числа в последовательности Фибоначи : ";
+	cout << m << endl;
+}
